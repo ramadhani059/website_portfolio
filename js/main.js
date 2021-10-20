632634a095ecc83d61c94ed00fa9d5464430f543
Pratama Ramadhani Wijaya <65058295+ramadhani059@users.noreply.github.com>
2021-10-20 18:29:44 +0700

    Add files via upload
    Membuat Website Portfolio

---
+++ js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+  "use strict";
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".scroll-to-bottom").fadeOut("slow");
+    } else {
+      $(".scroll-to-bottom").fadeIn("slow");
+    }
+  });
+
+  var portfolioIsotope = $(".portfolio-container").isotope({
+    itemSelector: ".portfolio-item",
+    layoutMode: "fitRows",
+  });
+  $("#portfolio-flters li").on("click", function () {
+    $("#portfolio-flters li").removeClass("active");
+    $(this).addClass("active");
+
+    portfolioIsotope.isotope({ filter: $(this).data("filter") });
+  });
+})(jQuery);
